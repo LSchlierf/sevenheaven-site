dfcca53bb0d20046597288a512f23c4867aefe9c
Lucas Schlierf <lucas.schlierf@tum.de>
2023-08-25 21:55:28 +0200

    added vidoe support to img cards

--- src/Components/ImageCard.js
+++ src/Components/ImageCard.js
@@ -12,51 +12,66 @@ function desktopPage(props) {
 
 function mobilePage(props) {
   const img = props.img || 'placeholder.png'
+  const vid = props.vid || 'https://www.youtube.com/embed/TApmI8YtYhc?si=xzFtA1pWDk0bj66u'
   const text = props.text || 'placeholder text lorem ipsum dolor sit amet'
   const fontSize = props.fontSize || 'larger'
   const backgroundColor = props.backgroundColor || 'black'
   const version = props.version || 'normal'
-  const height = { 'normal': '55%', 'portrait': '180%' }[version]
-  const textAlign = { 'normal': 'center', 'portrait': '' }[version]
+  const height = { 'normal': '55%', 'portrait': '180%', 'video': '55%' }[version]
+  const textAlign = { 'normal': 'center', 'portrait': '', 'video': 'center' }[version]
   const styleElement = {
     fontSize: fontSize,
     backgroundColor: backgroundColor,
     textAlign: textAlign
   }
-  return (
-    <div className='imageCardWrapperWrapper'>
-      {
-        version === 'portrait' ?
-          (
-            <div className='imageCardWrapper' style={{ paddingBottom: height }} >
-              <div className='imageCard'>
-                <div className='imageWrapper'>
-                  <img src={img} alt='card' />
-                </div>
-                <div className='caption'>
-                  <div className='textContainer' style={styleElement}>
-                    {text}
-                  </div>
-                </div>
+  if (version === 'portrait') {
+    return (
+      <div className='imageCardWrapperWrapper'>
+        <div className='imageCardWrapper' style={{ paddingBottom: height }} >
+          <div className='imageCard'>
+            <div className='imageWrapper'>
+              <img src={img} alt='card' />
+            </div>
+            <div className='caption'>
+              <div className='textContainer' style={styleElement}>
+                {text}
               </div>
             </div>
-          ) : (
-            <>
-              <div className='imageCardWrapper' style={{ paddingBottom: height }} >
-                <div className='imageCard'>
-                  <div className='imageWrapper'>
-                    <img src={img} alt='card' />
-                  </div>
-                </div>
-              </div>
-              <div className='caption'>
-                <div className='textContainer' style={styleElement}>
-                  {text}
-                </div>
-              </div>
-            </>
-          )
-      }
+          </div>
+        </div>
+      </div>
+    )
+  }
+  if (version === 'video') {
+    return (
+      <div className='imageCardWrapperWrapper'>
+        <div className='imageCardWrapper' style={{ paddingBottom: height }} >
+          <div className='imageCard'>
+            <iframe width="100%" height="100%" src={vid} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen />
+          </div>
+        </div>
+        <div className='caption'>
+          <div className='textContainer' style={styleElement}>
+            {text}
+          </div>
+        </div>
+      </div>
+    )
+  }
+  return (
+    <div className='imageCardWrapperWrapper'>
+      <div className='imageCardWrapper' style={{ paddingBottom: height }} >
+        <div className='imageCard'>
+          <div className='imageWrapper'>
+            <img src={img} alt='card' />
+          </div>
+        </div>
+      </div>
+      <div className='caption'>
+        <div className='textContainer' style={styleElement}>
+          {text}
+        </div>
+      </div>
     </div>
   )
 }
--- src/Pages/GalleryPage.js
+++ src/Pages/GalleryPage.js
@@ -27,11 +27,11 @@ function mobilePage() {
       <div className='subPageContent'>
         <Header text='Galerie' fontSize='9vw' paddingBottom='0'/>
         <Header text='Videos' fontSize='6vw' sub paddingBottom='5vw'/>
-        <ImageCard text={<>Major Tom<br/>JuHa Neuried - März 2023</>} fontSize='medium'/>
-        <ImageCard text={<>Disney Medley<br/>40 Jahre Westpark - Juli 2023</>} fontSize='medium'/>
-        <ImageCard text={<>Irgendwie, Irgendwo, Irgendwann<br/>Musikalisches Weinfest - Mai 2023</>} fontSize='medium'/>
-        <ImageCard text={<>Alles Neu<br/>40 Jahre westpark - Juli 2023</>} fontSize='medium'/>
-        <ImageCard text={<>Timewarp<br/>Musikalisches Weinfest - Mai 2023</>} fontSize='medium'/>
+        <ImageCard text={<>Major Tom<br/>JuHa Neuried - März 2023</>} fontSize='medium' version='video' vid='https://www.youtube.com/embed/dQw4w9WgXcQ?si=xWIxTEDuoBNsi3nL'/>
+        <ImageCard text={<>Disney Medley<br/>40 Jahre Westpark - Juli 2023</>} fontSize='medium' version='video' vid='https://www.youtube.com/embed/wpV-gGA4PSk?si=9QYx_u2Yh5CwOTVh'/>
+        <ImageCard text={<>Irgendwie, Irgendwo, Irgendwann<br/>Musikalisches Weinfest - Mai 2023</>} fontSize='medium' version='video'/>
+        <ImageCard text={<>Alles Neu<br/>40 Jahre westpark - Juli 2023</>} fontSize='medium' version='video'/>
+        <ImageCard text={<>Timewarp<br/>Musikalisches Weinfest - Mai 2023</>} fontSize='medium' version='video'/>
         <Header text='Fotos' fontSize='6vw' sub paddingBottom='5vw'/>
         <ImageCard text='40 Jahre Westpark' backgroundColor='darkred' fontSize='medium'/>
         <ImageCard text='Musikalisches Weinfest 2023' backgroundColor='darkred' fontSize='medium'/>

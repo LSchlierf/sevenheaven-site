c22facdbdbd01ee2b80562c19a2450448623f485
Lucas Schlierf <lucas.schlierf@tum.de>
2023-09-19 16:34:25 +0200

    fixed errors in repertoire sorting script

--- src/Pages/repertoire/updateRepertoire.py
+++ src/Pages/repertoire/updateRepertoire.py
@@ -1,30 +1,33 @@
 import json
 import string
 
-with open("Repertoire.txt", "r+") as f:
+with open("Repertoire.txt", "r") as f:
     repertoire = f.readlines()
+    # unify with trailing newlines
+    repertoire = list(map(lambda l : l.strip() + '\n', repertoire))
+    # sort out empty lines
+    repertoire = list(filter(lambda l : len(l.strip()) > 0, repertoire))
     # sort the list
     repertoire.sort()
     # remove duplicates
     repertoire = list(dict.fromkeys(repertoire))
-    #write back sorted list
-    f.seek(0)
+#write back sorted list
+with open("Repertoire.txt", "w") as f:
     f.writelines(repertoire)
-    f.truncate()
-    #initialize the dict for letter mapping
-    letters = {}
-    for i in string.ascii_uppercase:
-        letters[i] = []
-    letters["#"] = []
-    # map the songs to the letters
-    for song in repertoire:
-        title = song.strip().split(" - ")[0]
-        artist = song.strip().split(" - ")[1]
-        letter = title[0].upper()
-        if letter in string.ascii_uppercase:
-            letters[letter].append({"title": title, "artist": artist})
-        else:
-            letters["#"].append({"title": title, "artist": artist})
+#initialize the dict for letter mapping
+letters = {}
+for i in string.ascii_uppercase:
+    letters[i] = []
+letters["#"] = []
+# map the songs to the letters
+for song in repertoire:
+    title = song.strip().split(" - ")[0]
+    artist = song.strip().split(" - ")[1]
+    letter = title[0].upper()
+    if letter in string.ascii_uppercase:
+        letters[letter].append({"title": title, "artist": artist})
+    else:
+        letters["#"].append({"title": title, "artist": artist})
 
-    with open("Repertoire.json", "w") as r:
-        json.dump([{"letter": letter, "songs": letters[letter]} for letter in letters], r, indent=4)
+with open("Repertoire.json", "w") as r:
+    json.dump([{"letter": letter, "songs": letters[letter]} for letter in letters], r, indent=4)

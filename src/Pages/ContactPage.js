63ec41eddb0da5a82ad51836605459bbb5381538
Lucas Schlierf <lucas.schlierf@tum.de>
2024-01-07 12:43:08 +0100

    added burger menu to contact page

--- src/Pages/ContactPage.js
+++ src/Pages/ContactPage.js
@@ -10,6 +10,7 @@ import BackToMainPage from '../Components/BackToMainPage'
 import { IconContext } from 'react-icons/lib';
 import constants from './Constants.json'
 import { useLocation, useNavigate } from 'react-router-dom';
+import BurgerMenu from '../Components/BurgerMenu';
 
 // eslint-disable-next-line
 const mailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
@@ -173,6 +174,7 @@ function ContactPage() {
   return (
     <>
       <TitleBar />
+      <BurgerMenu />
       <div className='bgImgContainer'>
         <img srcSet={constants.imgSizes.map(size => '/img/' + size.toString() + '/' + constants.staticBg + ' ' + size.toString() + 'w').join(', ')} src={'/img/original/' + constants.staticBg} alt='background' />
       </div>
